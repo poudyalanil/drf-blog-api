a7369eed5b98686836034dd18b023671787a85b1
poudyalanil <anilrunjakote@gmail.com>
2021-04-05 18:25:14 +0545

    Class Based Views Single Blog Detail

--- blog/views.py
+++ blog/views.py
@@ -66,7 +66,7 @@ class BlogView(APIView):
         blogs = Blog.objects.all()
         serializer = Blog_Serializer(blogs,many=True)
         return Response(serializer.data)
-        
+
     def post(self,request):
         serializer = Blog_Serializer(data=request.data)
         if serializer.is_valid():
@@ -75,4 +75,28 @@ class BlogView(APIView):
         else:
             return Response(serializer.errors,status = status.HTTP_400_BAD_REQUEST)
 
+class SingleBlog(APIView):
+    def get_object(self,pk):
+        try:
+            blog = Blog.objects.get(pk=pk)
+        except Blog.DoesNotExist:
+            return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+    
+    def get(self,request,pk):
+        blog = self.get_object(pk)
+        serializer = Blog_Serializer(blog)
+        return Response(serializer.data)
+    
+    def put(self,request,pk):
+        blog =self.get_object(pk)
+        serializer = Blog_Serializer(blog,data=request.data)
 
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    def delete(self,request,pk):
+        blog = self.get_object(pk)
+        blog.delete()
+        return HttpResponse(status = status.HTTP_410_GONE)
