180133ab31684981a9ba67f753bc437c5be37645
poudyalanil <anilrunjakote@gmail.com>
2021-04-09 18:10:34 +0545

    Api View

--- blog/views.py
+++ blog/views.py
@@ -7,6 +7,9 @@ from django.http import HttpResponse, JsonResponse
 from rest_framework.parsers import JSONParser
 from .serializers import  Blog_Serializer,Category_Serializer,Tag_Serializer
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework.decorators import api_view
+from rest_framework.response import  Response
+from rest_framework import  status
 
 # Create your views here.
 def check_api(request):
@@ -18,44 +21,42 @@ def check_api(request):
         }
     return JsonResponse(resp)
 
-@csrf_exempt
+@api_view(['GET','POST'])
 def blog_list(request):
     if request.method=='GET':
         blogs = Blog.objects.all()
         serializer = Blog_Serializer(blogs,many=True)
-        return JsonResponse(serializer.data,safe=False)
+        return Response(serializer.data)
     elif  request.method=='POST':
-        data = JSONParser().parse(request)
-        serializer = Blog_Serializer(data=data)
+        serializer = Blog_Serializer(data=request.data)
 
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data,status = 201)
+            return Response(serializer.data,status = status.HTTP_201_CREATED)
         else:
-            return JsonResponse(serializer.errors,status = 400)
+            return Response(serializer.errors,status = status.HTTP_400_BAD_REQUEST)
 
-@csrf_exempt
+@api_view(['GET','PUT','DELETE'])
 def single_blog(request,pk):
     try:
         blog = Blog.objects.get(pk=pk)
     except Blog.DoesNotExist:
-        return HttpResponse(status=404)
+        return HttpResponse(status=status.HTTP_404_NOT_FOUND)
     
     if request.method == 'GET':
         serializer = Blog_Serializer(blog)
-        return JsonResponse(serializer.data,safe=False)
+        return Response(serializer.data)
     elif request.method =='PUT':
-        data = JSONParser().parse(request)
-        serializer = Blog_Serializer(blog,data=data)
+        serializer = Blog_Serializer(blog,data=request.data)
 
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data,status=201)
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
         else:
-            return JsonResponse(serializer.errors,status=400)
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
     elif request.method =='DELETE':
         blog.delete()
-        return HttpResponse(status = 410)
+        return HttpResponse(status = status.HTTP_410_GONE)
 
 
         
