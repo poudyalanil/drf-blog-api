7e51156f216e42abafa4cc51eb876c88f4c529a1
poudyalanil <anilrunjakote@gmail.com>
2021-04-22 13:10:53 +0545

    Blog List

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,11 @@
 from django.http.response import HttpResponse, JsonResponse
 from django.shortcuts import render
+from rest_framework import serializers
 from .models import Category, Blog, Tag
 from datetime import  datetime
 from django.http import HttpResponse, JsonResponse
 from rest_framework.parsers import JSONParser
+from .serializers import  Blog_Serializer,Category_Serializer,Tag_Serializer
 
 # Create your views here.
 def check_api(request):
@@ -14,4 +16,20 @@ def check_api(request):
         'country':'Nepal',
         }
     return JsonResponse(resp)
+
+def blog_list(request):
+    if request.get:
+        blogs = Blog.objects.all()
+        serializer = Blog_Serializer(blogs,many=True)
+        return JsonResponse(serializer.data,safe=False)
+    elif  request.post:
+        data = JSONParser().parse(request)
+        serializer = Blog_Serializer(data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data,status = 201)
+        else:
+            return JsonResponse(serializer.errors,status = 400)
+        
 
