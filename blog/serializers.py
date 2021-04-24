2a5faf52718657e026758dcca69156b6a3c3b92d
poudyalanil <anilrunjakote@gmail.com>
2021-04-24 13:02:10 +0545

    ModelSerializer

--- blog/serializers.py
+++ blog/serializers.py
@@ -1,26 +1,19 @@
 from rest_framework import serializers
 from .models import Blog,Tag,Category
 
-class Tag_Serializer(serializers.Serializer):
-    tag = serializers.CharField(max_length=255,default=None,blank=False,verbose_name="Tag Name")
-    date_created = serializers.DateTimeField(auto_now=True)
-    creator = serializers.CharField(max_length=255,default=None,blank=False,verbose_name="Creator")
-
-    def create(self,validated_data):
-        return Tag.objects.create(validated_data) 
-
-    def update(self, instance, validated_data):
-        instance.tag = validated_data.get('tag',instance.tag)
-        instance.date_created = validated_data.get('date_created',instance.date_created)
-        instance.creator = validated_data.get('creator',instance.creator)
-        instance.save()
-
-        return instance
+class Tag_Serializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = '__all__'
 
 
 class Category_Serializer(serializers.ModelSerializer):
-    pass
+    class Meta:
+        model = Category
+        fields = '__all__'
 
 class Blog_Serializer(serializers.ModelSerializer):
-    pass
+    class Meta:
+        model = Blog
+        fields = '__all__'
 
