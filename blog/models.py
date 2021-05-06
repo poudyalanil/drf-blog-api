2ab4ce6c5dc2371873de78a84adbf206e27d8eb3
poudyalanil <anilrunjakote@gmail.com>
2021-05-06 20:01:39 +0545

    Category and Tag Models

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,12 @@
 from django.db import models
 
 class Category(models.Model):
-    
+    category= models.CharField(max_length=255,default=None, blank=False,verbose_name="Category Name")
+    date_created = models.DateTimeField(auto_now=True)
+    creator = models.CharField(max_length=255,default=None,blank=False,verbose_name="Creator")
+
+
+class Tag(models.Model):
+    tag = models.CharField(max_length=255,default=None,blank=False,verbose_name="Tag Name")
+    date_created = models.DateTimeField(auto_now=True)
+    creator = models.CharField(max_length=255,default=None,blank=False,verbose_name="Creator")
