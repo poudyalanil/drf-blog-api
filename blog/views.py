1437f46983878de2c40be21ecf85fcd314193b2a
poudyalanil <anilrunjakote@gmail.com>
2021-05-06 11:46:10 +0545

    test

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,15 @@
 from django.http.response import HttpResponse, JsonResponse
 from django.shortcuts import render
 from .models import Category, Blog, Tag
+from datetime import  datetime
 
 # Create your views here.
 def check_api(request):
     resp = {
+        'date':datetime.now,
         "api_name":"drf-api",
         'version':"0.01",
         'status':"good"
+        
         }
     return JsonResponse(resp)
