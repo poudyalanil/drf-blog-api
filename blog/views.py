8de02d0f42176a3aa60c877d53fb6f1070809f84
poudyalanil <anilrunjakote@gmail.com>
2021-05-07 11:42:05 +0545

    Dummy Respnonse

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
+from django.http.response import HttpResponse, JsonResponse
 from django.shortcuts import render
+from .models import Category, Blog, Tag
 
 # Create your views here.
+def check_api(request):
+    resp = {
+        "api_name":"drf-api",
+        'version':"0.01",
+        'status':"good"
+        }
+    return JsonResponse(resp)
